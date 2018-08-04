ce2fc0520fe395bffea38c3eaf94c26a0a23193a
Michael O'Brien <mob@emobrien.com>
2018-08-04 15:48:45 +1000

    DEV: json logging

--- index.js
+++ index.js
@@ -1,136 +1,234 @@
 /*
     log.js - Simple, fast logging
+
+    usage(message, context, operations)
+
+        debug       - Current dev activity
+        error       - Any error
+        info        - Any non-error information
+        trace       - Persistent embedded (disabled by default) internal debug information
+        exception   - usage(message, err, context, operations)
+
+    Operations
+        alert       - Create Alert record
+        bug         - Create a Bug record
+        log         - Set to false to disable logging
+        notify      - Notify user via email template
+                      MOB - would be great to be able to notify via 'chat'
+        trail       - Add to account trail
+
+    Operation parameters
+        email       - Destination of notification
+        expires     - When alert expires
+        internal    - Extra internal information never displayed to the user
+        message     - If supplied, the original message param becomes params.subject
+        priority    - Alert priority
+        subject     - Message subject (alert, notice)
+        template    - Notification (email) template (implies notify)
+
+    Context Fields
+        source      - Originating source module
+        time        - Time of event
+        type        - debug|error|info|trace
+        req         - Request correlation ID
+        sessionId   - Cookie session ID
+        accountId   - Account ID
+        userId      - User ID
+        resourceId  - Resource ID
+        credId      - Credential ID
+        ip          - Client IP address
+        instance    - Instance ID
+        hostname    - Public DNS hostname
+        code        - Error code string
+        details     - Extra information not part of the message
  */
 
 export class Log {
-    constructor(options) {
-        global.log = this
+    constructor(options, context = {}) {
+        this.context = context
+        this.options = options
         this.loggers = []
-        this.output = []
-        this.type = { all: true }
-        this.from = { all: true }
+        this.filters = null
+        this.top = this
+        this.level = 0
         if (options == 'console') {
             this.addLogger(new DefaultLogger)
-            this.sync = true
         }
+    }
+
+    child(context) {
+        context = Object.assign({}, this.context, context)
+        let log = new Log(this.options, context)
+        log.loggers = this.loggers.slice(0)
+        log.top = this.top
+        return log
     }
 
     addLogger(logger) {
        this.loggers.push(logger)
     }
 
-    filter(type, from) {
-        this.type = {}
-        if (type) {
-            if (typeof type == 'string') {
-                type = [type]
-            }
-            for (let t of type) {
-                this.type[t] = true
-            }
+    setFilter(params) {
+        let top = this.top
+        top.level = params.level || top.level
+        if (!params.filter) {
+            return
         }
-        this.from = {}
-        if (from) {
-            if (typeof from == 'string') {
-                from = [from]
-            }
-            for (let f of from) {
-                this.from[f] = true
-            }
-        }
-    }
-
-    debug(from, ...msg) {
-        this.write('debug', from, ...msg)
-    }
-
-    error(from, ...msg) {
-        this.write('error', from, ...msg)
-    }
-
-    exception(from, e) {
-        let msg = []
-        if (e.message) {
-            msg.push(e.message)
-        }
-        if (e.errors) {
-            msg.push(JSON.stringify(e.errors))
-        }
-        if (e.stack) {
-            msg.push(e.stack)
-        }
-        if (typeof e == 'string') {
-            msg.push(e)
-        }
-        this.write('error', from, msg.join('\n'))
-    }
-
-    info(from, ...msg) {
-        this.write('info', from, ...msg)
-    }
-
-    trace(from, ...msg) {
-        this.write('trace', from, ...msg)
-    }
-
-    write(type, from, ...msg) {
-        if (!(this.type[type] || this.type.all)) {
-            return;
-        }
-        if (this.type['!' + type]) {
-            return;
-        }
-        if (!(this.from[from] || this.from.all)) {
-            return;
-        }
-        if (this.from['!' + from]) {
-            return;
-        }
-        msg = this.prepMsg(msg)
-        this.output.push([type, from, msg])
-        if (this.sync) {
-            this.flush()
-        } else if (!this.scheduled) {
-            this.scheduled = true
-            let self = this
-            if (global.process && global.process.nextTick) {
-                process.nextTick(function() {
-                    self.flush()
-                })
-            } else {
-                setTimeout(function() {
-                    self.flush()
-                }, 0)
-            }
-        }
-    }
-
-    flush() {
-        this.scheduled = false
-        if (this.output.length > 0) {
-            try {
-                for (let logger of this.loggers) {
-                    logger.write(this.output)
+        top.filters = {}
+        if (typeof params.filter == 'string') {
+            /*
+                --log key=value:level,value:-1/... (Level defaults to 4)
+             */
+            for (let match of params.filter.split('/')) {
+                let [key, values] = match.split('=')
+                let item = top.filters[key] = {}
+                for (let v of values.split(',')) {
+                    let [value, level] = v.split(':')
+                    item[value] = level || 4
                 }
-            } catch (e) {
-                console.log(e)
             }
-            this.output = []
+        } else {
+            top.filters = params.filters
+        }
+        top.filterEntries = Object.entries(top.filters || {})
+    }
+
+    addContext(context) {
+        this.context = Object.assign(this.context, context)
+    }
+
+    /*
+        source, message, context
+        WARNING: legacy will modify submitted contexts
+     */
+    legacy(source, context, ops) {
+        let message = context
+        context = ops || {}
+        ops = {}
+        for (let field of ['alert', 'bug', 'log', 'notify', 'trail']) {
+            if (context[field]) {
+                ops[field] = context[field]
+                context[field] = undefined
+            }
+        }
+        context.source = source
+        context.legacy = true
+        return {context, message, ops}
+    }
+
+    debug(message, context, ops) {
+        if (context != undefined && typeof context == 'string') {
+            ({context, ops} = this.legacy(message, context, ops))
+        }
+        this.submit('debug', message, context, ops)
+    }
+
+    error(message, context, ops) {
+        if (message.indexOf("Cannot read property 'split'") >= 0) {
+            context = context || {}
+            context.stack = (new Error(message)).stack
+        }
+        if (context != undefined && typeof context == 'string') {
+            ({context, message, ops} = this.legacy(message, context, ops))
+        }
+        this.submit('error', message, context, ops)
+    }
+
+    exception(message, err, context = {}, ops = {}) {
+        context.exception = err
+        this.submit('exception', message, context, ops)
+    }
+
+    info(message, context, ops) {
+        if (context != undefined && typeof context == 'string') {
+            ({context, message, ops} = this.legacy(message, context, ops))
+        }
+        this.submit('info', message, context, ops)
+    }
+
+    trace(message, context = {}, ops = {}) {
+        if (context != undefined && typeof context != 'object') {
+            ({context, message, ops} = this.legacy(message, context, ops))
+        }
+        context.level = context.level || 5
+        this.submit('trace', message, context, ops)
+    }
+
+    submit(type, message, context = {}, ops = {}) {
+        if (context.message) {
+            context.subject = message
+        } else {
+            context.message = message
+        }
+        context.type = type
+        context.level = context.level || 0
+        this.write({context, ops})
+    }
+
+    write(params) {
+        this.prep(params)
+        for (let logger of this.loggers) {
+            logger.write(params)
         }
     }
 
-    prepMsg(msg) {
-        let items = []
-        for (let item of msg) {
-            if (typeof item == 'string') {
-                items.push(item)
-            } else if (item instanceof Error) {
-                items.push(item.toString())
-            } else {
-                items.push(JSON.stringify(item, null, 4))
+    filter(params) {
+        let top = this.top
+        let {context} = params
+        params.context = context = Object.assign({}, this.context, context)
+        let level = top.level
+
+        if (top.filters) {
+            for (let [key, item] of top.filterEntries) {
+                let thisLevel = (context[key]) ? item[context[key]] : -1
+                if (thisLevel < 0) {
+                    return false
+                } else {
+                    level = Math.max(level, thisLevel)
+                }
             }
         }
-        return items.join(' ')
+        if (context.level > level) {
+            return false
+        }
+        return true
+    }
+
+    prep(params) {
+        let {context, ops} = params
+        let message = context.message
+
+        if (context instanceof Error) {
+            let exception = context
+            context = {exception}
+            context.message = exception.message
+
+        } else if (message instanceof Error) {
+            let exception = message
+            context.exception = exception
+            context.message = exception.message
+
+        } else if (typeof message != 'string') {
+            context.message = JSON.stringify(message)
+        }
+        if (context.exception) {
+            let err = context.exception
+            let exception = context.exception = Object.assign({}, err)
+            if (err.stack) {
+                exception.stack = err.stack.split('\n').map(s => s.trim())
+            }
+            exception.message = err.message
+            exception.code = err.code
+        }
+        //  MOB - push back into application
+        if (context.template) {
+            ops.notify = true
+        }
+        context.time = new Date()
+        context.cutoff = this.top.level
+        if (Array.isArray(context.message)) {
+            context.message = context.message.join(' ')
+        }
     }
 
     addBrowserExceptions() {
@@ -161,9 +259,14 @@ export class Log {
 }
 
 export class DefaultLogger {
-    write(output) {
-        for (let item of output) {
-            console.log(item[0], item[1] + ':', item[2])
+    write(params) {
+        let {context, ops} = params
+        let {message, source, type} = context
+        if (context.error || context.type == 'trace') {
+            console.log(`${source}: ${type}: ${message}`)
+            console.log(JSON.stringify(context, null, 4) + '\n')
+        } else {
+            console.log(`${source}: ${type}: ${message}`)
         }
     }
 }
